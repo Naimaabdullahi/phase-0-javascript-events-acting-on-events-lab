377618028ac06e3b070900108f4588516297721a
Naimaabdullahi <naima.noor@student.moringaschool.com>
2025-03-18 14:17:08 +0300

    arrow

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-// Your code here
-const dodger = document.getElementById("dodger");
+const dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px"
+dodger.style.left = "100px"
+
+document.addEventListener("keydown", function (e) {
+  if(e.key === "ArrowLeft")
+    return moveDodgerLeft();
+});
 
 function moveDodgerLeft() {
-  const left = parseInt(dodger.style.left.replace("px", ""), 10);
-  if (left > 0) {
-    dodger.style.left = `${left - 10}px`;
+  const leftNumbers = dodger.style.left.replace("px", " ")
+  const left = parseInt(leftNumbers,10)
+
+  if(left > 0) {
+    dodger.style.left = `${left - 1}px`
   }
 }
 
+document.addEventListener("keydown", function (e){
+  if(e.key === "ArrowRight") {
+    return moveDodgerRight()
+  }
+})
+
 function moveDodgerRight() {
-  const left = parseInt(dodger.style.left.replace("px", ""), 10);
-  if (left < 360) { // Assuming the game's width is 400px
-    dodger.style.left = `${left + 10}px`;
+  const rightNumbers = dodger.style.left.replace('px',' ')
+  const left = parseInt(rightNumbers, 10) 
+
+  if(left > 0) {
+    dodger.style.left = `${left + 1}px`
   }
 }
